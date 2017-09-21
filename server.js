67d79a238753cf340d729177cebb251bd4b558ad
Webnza <kapoor.anil749@gmail.com>
2017-09-21 23:20:55 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const Hapi = require('hapi');
+const Good = require('good');
+
+const server = new Hapi.Server();
+server.connection({ port: 3000, host: 'localhost' });
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('<div style="margin: 20% 30%"><h1>Welcome to the Hapi Rest API Service </h1></div>');
+    }
+});
+
+server.register([{
+    register: Good,
+    options: {
+        reporters: {
+            console: [{
+                module: 'good-squeeze',
+                name: 'Squeeze',
+                args: [{
+                    response: '*',
+                    log: '*'
+                }]
+            }, {
+                module: 'good-console'
+            }, 'stdout']
+        }
+    }
+},
+require('./src/routes/users')
+], (err) => {
+
+    if (err) {
+        throw err;
+    }
+
+    server.start((err) => {
+        if (err) {
+            throw err;
+        }
+        server.log('info', 'Server running at: ' + server.info.uri);
+    });
+});
