fadf1563b591a4fbb4acb2ea7f4213c2f0cc029e
Webnza <kapoor.anil749@gmail.com>
2017-09-21 23:34:24 +0530

    Add files via upload

--- src/models/users.js
+++ src/models/users.js
@@ -1,1 +1,81 @@
+'use strict';
 
+const Boom = require('boom');  
+const uuid = require('node-uuid');  
+
+const mongojs = require('mongojs'); 
+const db = mongojs('HapiRESTAPI', ['users']);
+
+function UsersModel(){};
+
+/* Get All Users */
+UsersModel.prototype.getAllUsers = function(request, reply) {    
+   db.users.find((err, docs) => {
+        if (err) {
+            return reply(Boom.wrap(err, 'Internal MongoDB error'));
+        }
+        reply(docs);
+    });
+};
+
+/* Get Single User */
+UsersModel.prototype.getUser = function(request, reply) {    
+   db.users.findOne({
+        _id: request.params.id
+    }, (err, doc) => {
+        if (err) {
+            return reply(Boom.wrap(err, 'Internal MongoDB error'));
+        }
+        if (!doc) {
+            return reply(Boom.notFound());
+        }
+        reply(doc);
+    });
+};
+
+/* Add Single User */
+UsersModel.prototype.addUser = function(request, reply) {    
+    const user = request.payload;
+    user._id = uuid.v1();
+
+    db.users.save(user, (err, result) => {
+        if (err) {
+            return reply(Boom.wrap(err, 'Internal MongoDB error'));
+        }
+        reply(user);
+    });
+};
+
+/* Update Single User */
+UsersModel.prototype.updateUser = function(request, reply) {    
+    db.users.update({
+        _id: request.params.id
+    }, {
+        $set: request.payload
+    }, function (err, result) {
+        if (err) {
+            return reply(Boom.wrap(err, 'Internal MongoDB error'));
+        }
+        if (result.n === 0) {
+            return reply(Boom.notFound());
+        }
+        reply(result).code(200);
+    });
+};
+
+/* Delete Single User */
+UsersModel.prototype.destroyUser = function(request, reply) {    
+    db.users.remove({
+        _id: request.params.id
+    }, function (err, result) {
+        if (err) {
+            return reply(Boom.wrap(err, 'Internal MongoDB error'));
+        }
+        if (result.n === 0) {
+            return reply(Boom.notFound());
+        }
+        reply(result).code(200);
+    });
+};
+
+module.exports = UsersModel;
